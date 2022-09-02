de8f077c61c0c1584da8308a1dc27e61f5edba8f
Josh <jbyrd133402@gmail.com>
2022-09-02 14:55:15 -0500

    forgot a file?

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -3,6 +3,10 @@ import "./Gallery.css";
 const Gallery = ({ title, imagePairs }) => {
   return (
     <div className="gallery">
+      <h1>{title}</h1>
+      {links.map((link) => {
+          return <a href={link.href}>{link.content}</a>;
+        })}
     </div>
   );
 };
