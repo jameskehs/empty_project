56983c6a7e393e8f8c21370d2e9acfa0877c02b9
Josh Byrd <jbyrd133402@gmail.com>
2022-09-09 16:50:16 -0500

    Edit Panel setup
    Go to cats page and click on the collection and youll see it fill the edit panel with an input field, to demonstrate functionality. Also go to App.js and read comments to turn off editpanel for now

--- src/Components/EditPanel/EditPanel.jsx
+++ src/Components/EditPanel/EditPanel.jsx
@@ -1,53 +1,63 @@
 import "./EditPanel.css";
 
 const EditPanel = (props) => {
-  switch (props.componentName) {
-    case "Hero":
-      {
-        const { title, body, imgSrc, buttons } = props;
-        return (
-          <div className="EditPanel">
-            <p>{title}</p>
-          </div>
-        );
-      }
-      break;
-    case "Collection":
-      {
-        const { Title, setTitle } = props;
-        return (
-          <div className="EditPanel">
-            <input
-              value={Title}
-              type="Text"
-              placeholder="Title"
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-        );
-      }
-      break;
-    case "Contact":
-      {
-        const { title, body, imgSrc, buttons } = props;
-        return (
-          <div className="EditPanel">
-            <p>{title}</p>
-          </div>
-        );
-      }
-      break;
-    case "Gallery":
-      {
-        const { title, body, imgSrc, buttons } = props;
-        return (
-          <div className="EditPanel">
-            <p>{title}</p>
-          </div>
-        );
-      }
-      break;
+  function ShowComponentProperties(props) {
+    switch (props.componentName) {
+      case "Hero":
+        {
+          const { Title, body, imgSrc, buttons } = props;
+          return (
+            <div className="EditPanel">
+              <p>{Title}</p>
+            </div>
+          );
+        }
+        break;
+      case "Collection":
+        {
+          const { Title, setTitle } = props;
+          return (
+            <div className="EditPanel">
+              <input
+                value={Title}
+                type="Text"
+                placeholder="Title"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+          );
+        }
+        break;
+      case "Contact":
+        {
+          const { email, phone, address } = props;
+          return (
+            <div className="EditPanel">
+              <p>{email}</p>
+            </div>
+          );
+        }
+        break;
+      case "Gallery":
+        {
+          const { Title, imagePairs } = props;
+          return (
+            <div className="EditPanel">
+              <p>{Title}</p>
+            </div>
+          );
+        }
+        break;
+    }
   }
+
+  return (
+    <div className="sidenav">
+      <h1>Edit Panel</h1>
+      <p>Welcome! Make changes to your website here!</p>
+      {ShowComponentProperties(props)}
+    </div>
+  );
 };
 
 export default EditPanel;
