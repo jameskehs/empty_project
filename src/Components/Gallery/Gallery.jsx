b4a39c98532a8662fba67f3776a31ea4e77d14bb
jameskehs <jameskehsdev@gmail.com>
2022-09-14 10:57:37 -0500

    Gallery Improvements

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,19 +1,33 @@
 import "./Gallery.css";
 import { useState } from "react";
+import ImageOverlay from "./ImageOverlay";
 
 const Gallery = (props) => {
   console.log(props);
   const [title, setTitle] = useState(props.title);
   const [images, setImages] = useState(props.images);
 
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(undefined);
+
   return (
     <div className="gallery">
+      {isOverlayOpen && (
+        <ImageOverlay setIsOverlayOpen={setIsOverlayOpen} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} images={images} />
+      )}
       <h3>{title}</h3>
       <div className="gallery-img-container">
         {images.map((image, index) => {
           const { imgSrc, imgSubtitle } = image;
           return (
-            <div className="gallery-img-pair" key={index}>
+            <div
+              className="gallery-img-pair"
+              key={index}
+              onClick={() => {
+                setIsOverlayOpen(true);
+                setSelectedIndex(index);
+              }}
+            >
               <img src={imgSrc} alt={imgSubtitle} />
               <p>{imgSubtitle}</p>
             </div>
